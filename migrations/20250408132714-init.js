31d6554cfc2384d2a52dec88a50937b070e48c1a
julian <juliankimmings@gmail.com>
2025-05-08 14:00:57 +0100

    update migrations to match workflow fixture

--- migrations/20250408132714-init.js
+++ migrations/20250408132714-init.js
@@ -6,77 +6,72 @@ export const up = async (db) => {
   await db.collection("workflows").insertOne({
     code: "frps-private-beta",
     payloadDefinition: {
-      $id: "https://fg-cw.com/grant-application.schema.json",
-      type: "object",
-      properties: {
-        clientRef: {
-          type: "string"
-        },
-        code: {
-          type: "string"
-        },
-        createdAt: {
-          type: "object"
-        },
-        submittedAt: {
-          type: "object"
-        },
-        identifiers: {
-          type: "object",
-          properties: {
-            sbi: {
-              type: "string"
-            },
-            frn: {
-              type: "string"
-            },
-            crn: {
-              type: "string"
-            },
-            defraId: {
-              type: "string"
-            }
+      identifiers: {
+        type: "object",
+        properties: {
+          sbi: {
+            type: "string",
+            label: "SBI"
+          },
+          frn: {
+            type: "string",
+            label: "FRN"
+          },
+          crn: {
+            type: "string",
+            label: "CRN"
+          },
+          defraId: {
+            type: "string",
+            label: "Defra ID"
           }
-        },
-        answers: {
-          type: "object",
-          properties: {
-            scheme: {
-              type: "string"
-            },
-            year: {
-              type: "integer",
-              minimum: 2000,
-              maximum: 2100
-            },
-            hasCheckedLandIsUpToDate: {
-              type: "boolean"
-            },
-            actionApplications: {
-              type: "array",
-              items: {
-                type: "object",
-                properties: {
-                  parcelId: {
-                    type: "string"
-                  },
-                  sheetId: {
-                    type: "string"
-                  },
-                  code: {
-                    type: "string"
-                  },
-                  appliedFor: {
-                    type: "object",
-                    properties: {
-                      unit: {
-                        type: "string",
-                        enum: ["ha", "acres", "sqm", "sqft"]
-                      },
-                      quantity: {
-                        type: "number",
-                        minimum: 0
-                      }
+        }
+      },
+      answers: {
+        type: "object",
+        properties: {
+          scheme: {
+            type: "string",
+            label: "Scheme type"
+          },
+          year: {
+            type: "number",
+            minimum: 2000,
+            maximum: 2100,
+            label: "Year"
+          },
+          hasCheckedLandIsUpToDate: {
+            type: "boolean",
+            label: "Has checked land is up to date?"
+          },
+          actionApplications: {
+            type: "array",
+            label: "Action applications",
+            items: {
+              type: "object",
+              properties: {
+                parcelId: {
+                  type: "string",
+                  label: "Parcel ID"
+                },
+                sheetId: {
+                  type: "string",
+                  label: "Sheet ID"
+                },
+                code: {
+                  type: "string",
+                  label: "Code"
+                },
+                appliedFor: {
+                  type: "object",
+                  properties: {
+                    unit: {
+                      type: "string",
+                      enum: ["ha", "acres", "sqm", "sqft"]
+                    },
+                    quantity: {
+                      type: "number",
+                      minimum: 0
                     }
                   }
                 }
