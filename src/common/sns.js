38380a6561e48192f1189848e5dd850a9891c4e4
Islas Ahmed Nawaz <islas104@gmail.com>
2025-05-13 14:59:56 +0100

    Wrap SNS message in CloudEvents-compliant envelope for case stage update publishing

--- src/common/sns.js
+++ src/common/sns.js
@@ -1,15 +1,24 @@
 import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 import { config } from "../config.js";
+import { randomUUID } from "crypto";
 
-const region = config?.aws?.awsRegion || "eu-west-2";
-const endpoint = config?.aws?.snsEndpoint || "http://localhost:4566";
+const region = config.get("aws.awsRegion") || "eu-west-2";
+const endpoint = config.get("aws.snsEndpoint") || "http://localhost:4566";
 
 export const snsClient = new SNSClient({ region, endpoint });
 
 export const publish = async (topicArn, message) => {
+  const event = {
+    id: randomUUID(),
+    source: config.get("serviceName"),
+    specVersion: "1.0",
+    type: `cloud.defra.${config.get("cdpEnvironment")}.${config.get("serviceName")}.case.stage.updated`,
+    data: message
+  };
+
   return snsClient.send(
     new PublishCommand({
-      Message: JSON.stringify(message),
+      Message: JSON.stringify(event),
       TopicArn: topicArn
     })
   );
