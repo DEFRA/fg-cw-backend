79406ccefeb9b4f1b4d165904a835ed16eb082d1
julian <juliankimmings@gmail.com>
2025-06-04 14:58:56 +0100

    update vitest integration

--- test/vitest.config.js
+++ test/vitest.config.js
@@ -4,8 +4,8 @@ const CW_PORT = 3801;
 const MONGO_PORT = 28017;
 const AWS_PORT = 4866;
 const REDIS_PORT = 6879;
-const SQS_ENDPOINT = "http://localhost.localstack.cloud:4866";
-const SNS_ENDPOINT = "http://localhost.localstack.cloud:4866";
+const SQS_ENDPOINT = "http://localhost:4866";
+const SNS_ENDPOINT = "http://localhost:4866";
 const CREATE_NEW_CASE_SQS_URL =
   "http://sqs.eu-west-2.127.0.0.1:4566/000000000000/create_new_case";
 const LOCALSTACK_ENABLED = true;
