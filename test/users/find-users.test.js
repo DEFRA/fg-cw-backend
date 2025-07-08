395578113ddadb0b2d3010629c90663491ef28cd
Justin Ramel <justin.ramel@gmail.com>
2025-07-08 11:06:46 +0100

    FUT-502 assign user to case (#144)
    * FUT-502 assign user to case
    
    - add allAppRoles to findAll endpoint
    - add anyAppRoles to findAll endpoint
    - update user repository to filter on allAppRoles
    - update user repository to filter on anyAppRoles
    
    https://eaflood.atlassian.net/browse/FUT-502
    
    * - add integration tests for allAppRoles and any AppRoles

--- test/users/find-users.test.js
+++ test/users/find-users.test.js
@@ -127,4 +127,245 @@ describe("GET /users", () => {
       ],
     });
   });
+
+  it("returns users filtered by allAppRoles", async () => {
+    await wreck.post("/users", {
+      payload: {
+        idpId: "abcd1234-5678-90ab-cdef-1234567890ab",
+        name: "Name",
+        email: "name.surname@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE", "ROLE_RPA_ADMIN"],
+      },
+    });
+
+    await wreck.post("/users", {
+      payload: {
+        idpId: "7467b7e2-1022-45fd-9e81-ab364206de40",
+        name: "Another",
+        email: "another.user@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE"],
+      },
+    });
+
+    const searchParams = new URLSearchParams();
+    searchParams.append("allAppRoles", "ROLE_RPA_CASES_APPROVE");
+    searchParams.append("allAppRoles", "ROLE_RPA_ADMIN");
+
+    const response = await wreck.get(`/users?${searchParams}`);
+
+    expect(response).toEqual({
+      res: expect.objectContaining({
+        statusCode: 200,
+      }),
+      payload: [
+        {
+          id: expect.any(String),
+          idpId: "abcd1234-5678-90ab-cdef-1234567890ab",
+          name: "Name",
+          email: "name.surname@defra.gov.uk",
+          idpRoles: ["defra-idp"],
+          appRoles: ["ROLE_RPA_CASES_APPROVE", "ROLE_RPA_ADMIN"],
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        },
+      ],
+    });
+  });
+
+  it("returns users filtered by anyAppRoles", async () => {
+    await wreck.post("/users", {
+      payload: {
+        idpId: "abcd1234-5678-90ab-cdef-1234567890ab",
+        name: "Name",
+        email: "name.surname@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE", "ROLE_RPA_ADMIN", "ROLE_ANY_OF"],
+      },
+    });
+
+    await wreck.post("/users", {
+      payload: {
+        idpId: "7467b7e2-1022-45fd-9e81-ab364206de40",
+        name: "Another",
+        email: "another.user@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE"],
+      },
+    });
+
+    const searchParams = new URLSearchParams();
+    searchParams.append("anyAppRoles", "ROLE_ANY_OF");
+
+    const response = await wreck.get(`/users?${searchParams}`);
+
+    expect(response).toEqual({
+      res: expect.objectContaining({
+        statusCode: 200,
+      }),
+      payload: [
+        {
+          id: expect.any(String),
+          idpId: "abcd1234-5678-90ab-cdef-1234567890ab",
+          name: "Name",
+          email: "name.surname@defra.gov.uk",
+          idpRoles: ["defra-idp"],
+          appRoles: ["ROLE_RPA_CASES_APPROVE", "ROLE_RPA_ADMIN", "ROLE_ANY_OF"],
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        },
+      ],
+    });
+  });
+
+  it("returns users filtered by allAppRoles and anyAppRoles", async () => {
+    await wreck.post("/users", {
+      payload: {
+        idpId: "abcd1234-5678-90ab-cdef-1234567890ab",
+        name: "Name",
+        email: "name.surname@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE", "ROLE_RPA_ADMIN", "ROLE_ANY_OF"],
+      },
+    });
+
+    await wreck.post("/users", {
+      payload: {
+        idpId: "7467b7e2-1022-45fd-9e81-ab364206de40",
+        name: "Another",
+        email: "another.user@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE"],
+      },
+    });
+
+    const searchParams = new URLSearchParams();
+    searchParams.append("allAppRoles", "ROLE_RPA_CASES_APPROVE");
+    searchParams.append("allAppRoles", "ROLE_RPA_ADMIN");
+    searchParams.append("anyAppRoles", "ROLE_ANY_OF");
+
+    const response = await wreck.get(`/users?${searchParams}`);
+
+    expect(response).toEqual({
+      res: expect.objectContaining({
+        statusCode: 200,
+      }),
+      payload: [
+        {
+          id: expect.any(String),
+          idpId: "abcd1234-5678-90ab-cdef-1234567890ab",
+          name: "Name",
+          email: "name.surname@defra.gov.uk",
+          idpRoles: ["defra-idp"],
+          appRoles: ["ROLE_RPA_CASES_APPROVE", "ROLE_RPA_ADMIN", "ROLE_ANY_OF"],
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        },
+      ],
+    });
+  });
+
+  it("returns no users when no match in allAppRoles", async () => {
+    await wreck.post("/users", {
+      payload: {
+        idpId: "abcd1234-5678-90ab-cdef-1234567890ab",
+        name: "Name",
+        email: "name.surname@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE", "ROLE_RPA_ADMIN", "ROLE_ANY_OF"],
+      },
+    });
+
+    await wreck.post("/users", {
+      payload: {
+        idpId: "7467b7e2-1022-45fd-9e81-ab364206de40",
+        name: "Another",
+        email: "another.user@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE"],
+      },
+    });
+
+    const searchParams = new URLSearchParams();
+    searchParams.append("allAppRoles", "ROLE_NO_MATCH");
+
+    const response = await wreck.get(`/users?${searchParams}`);
+
+    expect(response).toEqual({
+      res: expect.objectContaining({
+        statusCode: 200,
+      }),
+      payload: [],
+    });
+  });
+
+  it("returns no users when no match in anyAppRoles", async () => {
+    await wreck.post("/users", {
+      payload: {
+        idpId: "abcd1234-5678-90ab-cdef-1234567890ab",
+        name: "Name",
+        email: "name.surname@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE", "ROLE_RPA_ADMIN", "ROLE_ANY_OF"],
+      },
+    });
+
+    await wreck.post("/users", {
+      payload: {
+        idpId: "7467b7e2-1022-45fd-9e81-ab364206de40",
+        name: "Another",
+        email: "another.user@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE"],
+      },
+    });
+
+    const searchParams = new URLSearchParams();
+    searchParams.append("anyAppRoles", "ROLE_NO_MATCH");
+
+    const response = await wreck.get(`/users?${searchParams}`);
+
+    expect(response).toEqual({
+      res: expect.objectContaining({
+        statusCode: 200,
+      }),
+      payload: [],
+    });
+  });
+
+  it("returns no users when no match in allAppRoles and no match in anyAppRoles", async () => {
+    await wreck.post("/users", {
+      payload: {
+        idpId: "abcd1234-5678-90ab-cdef-1234567890ab",
+        name: "Name",
+        email: "name.surname@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE", "ROLE_RPA_ADMIN", "ROLE_ANY_OF"],
+      },
+    });
+
+    await wreck.post("/users", {
+      payload: {
+        idpId: "7467b7e2-1022-45fd-9e81-ab364206de40",
+        name: "Another",
+        email: "another.user@defra.gov.uk",
+        idpRoles: ["defra-idp"],
+        appRoles: ["ROLE_RPA_CASES_APPROVE"],
+      },
+    });
+
+    const searchParams = new URLSearchParams();
+    searchParams.append("allAppRoles", "ROLE_NO_MATCH_ALL");
+    searchParams.append("anyAppRoles", "ROLE_NO_MATCH_ANY");
+
+    const response = await wreck.get(`/users?${searchParams}`);
+
+    expect(response).toEqual({
+      res: expect.objectContaining({
+        statusCode: 200,
+      }),
+      payload: [],
+    });
+  });
 });
