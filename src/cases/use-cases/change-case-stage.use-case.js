744f23b4fecdb36509e22bd38481598b1dff93cd
Aditya Kolnati <aditya.kolnati@gmail.com>
2025-07-08 10:38:14 +0100

    updated pmf workflow definition (#137)
    * pigs might fly workflow definition
    
    * updated the workflow definition to allow for conditional rendering
    
    * updated workflow and case files
    
    * failing test fixed
    
    * current stage and next stage changed
    
    ---------
    
    Co-authored-by: Aditya Kolnati <adityakolnati@adityas.mac.pro.m2.max.chip.lan>

--- src/cases/use-cases/change-case-stage.use-case.js
+++ src/cases/use-cases/change-case-stage.use-case.js
@@ -1,3 +1,5 @@
+import Boom from "@hapi/boom";
+
 import { publishCaseStageUpdated } from "../publishers/case-event.publisher.js";
 import { updateStage } from "../repositories/case.repository.js";
 import { findCaseByIdUseCase } from "./find-case-by-id.use-case.js";
@@ -5,7 +7,22 @@ import { findCaseByIdUseCase } from "./find-case-by-id.use-case.js";
 export const changeCaseStageUseCase = async (caseId) => {
   const kase = await findCaseByIdUseCase(caseId);
 
-  const nextStage = "contract";
+  const currentStageIndex = kase.stages.findIndex(
+    (stage) => stage.id === kase.currentStage,
+  );
+
+  // If current stage is not found or is the last stage, throw error
+  if (
+    currentStageIndex === -1 ||
+    currentStageIndex === kase.stages.length - 1
+  ) {
+    throw Boom.notFound(
+      `Cannot progress case ${caseId} from stage ${kase.currentStage}`,
+    );
+  }
+
+  // Get the next stage ID
+  const nextStage = kase.stages[currentStageIndex + 1].id;
 
   await updateStage(caseId, nextStage);
 
