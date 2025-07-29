54bcdb300a1816d2accc1b60dcb3097403c090ad
Aditya Kolnati <adityakolnati@adityas.mac.pro.m2.max.chip.lan>
2025-07-29 12:05:36 +0100

    refactored the code to remove duplicated blocks

--- src/cases/models/workflow.js
+++ src/cases/models/workflow.js
@@ -1,4 +1,5 @@
 import { ObjectId } from "mongodb";
+import { createWorkflowMockData } from "./workflow-mock-data.js";
 
 export class Workflow {
   constructor(props) {
@@ -11,92 +12,8 @@ export class Workflow {
 
   static createMock(props) {
     return new Workflow({
-      code: "workflow-code",
-      pages: this.createMockPages(),
-      stages: this.createMockStages(),
-      requiredRoles: this.createMockRequiredRoles(),
+      ...createWorkflowMockData(),
       ...props,
     });
-  }
-
-  static createMockPages() {
-    return {
-      cases: {
-        details: {
-          banner: {
-            title: {
-              ref: "$.payload.businessName",
-              type: "string",
-            },
-            summary: {
-              clientReference: {
-                label: "Client Reference",
-                ref: "$.payload.clientRef",
-                type: "string",
-              },
-            },
-          },
-          tabs: {
-            caseDetails: {
-              title: "Application",
-              sections: [
-                {
-                  title: "Details",
-                  type: "list",
-                  fields: [
-                    {
-                      ref: "$.payload.answers.field1",
-                      type: "string",
-                      label: "Field 1",
-                    },
-                  ],
-                },
-              ],
-            },
-          },
-        },
-      },
-    };
-  }
-
-  static createMockStages() {
-    return [
-      {
-        id: "stage-1",
-        title: "Stage 1",
-        taskGroups: [
-          {
-            id: "stage-1-tasks",
-            title: "Stage 1 Tasks",
-            tasks: [
-              {
-                id: "task-1",
-                title: "Task 1",
-                type: "boolean",
-              },
-            ],
-          },
-        ],
-        actions: [
-          {
-            id: "action-1",
-            label: "Action 1",
-          },
-        ],
-      },
-      {
-        id: "stage-2",
-        title: "Stage 2",
-        taskGroups: [],
-        actions: [],
-      },
-    ];
-  }
-
-  static createMockRequiredRoles() {
-    return {
-      allOf: ["ROLE_1", "ROLE_2"],
-      anyOf: ["ROLE_3"],
-    };
   }
 }
