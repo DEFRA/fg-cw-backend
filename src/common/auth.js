822fde450b120ce71d06c84ce223b3bfa84a3104
julian <juliankimmings@gmail.com>
2025-07-30 12:14:00 +0100

    adds RPA_ADMIN role

--- src/common/auth.js
+++ src/common/auth.js
@@ -1,6 +1,7 @@
 // TODO: this is a stub. Replace when auth has been implemented.
 export const getAuthenticatedUserRoles = () => [
   "ROLE_RPA",
+  "ROLE_RPA_ADMIN",
   "ROLE_FLYING_PIGS",
   "ROLE_1",
   "ROLE_2",
--- src/common/auth.test.js
+++ src/common/auth.test.js
@@ -5,6 +5,7 @@ describe("auth - getAuthenticatedUserRoles", () => {
   it("should return roles", () => {
     expect(getAuthenticatedUserRoles()).toEqual([
       "ROLE_RPA",
+      "ROLE_RPA_ADMIN",
       "ROLE_FLYING_PIGS",
       "ROLE_1",
       "ROLE_2",
