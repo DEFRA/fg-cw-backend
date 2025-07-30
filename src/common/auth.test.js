900d86908aaf94a1d434e5c1d0f57fbefdc2efbe
julian <juliankimmings@gmail.com>
2025-07-30 11:59:31 +0100

    update auth test

--- src/common/auth.test.js
+++ src/common/auth.test.js
@@ -6,8 +6,9 @@ describe("auth - getAuthenticatedUserRoles", () => {
     expect(getAuthenticatedUserRoles()).toEqual([
       "ROLE_RPA",
       "ROLE_FLYING_PIGS",
-      "ROLE_LEVEL_1",
-      "ROLE_LEVEL_2",
+      "ROLE_1",
+      "ROLE_2",
+      "ROLE_3",
     ]);
   });
 });
