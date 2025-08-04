9bd0ac2ae0ce5e8804ae18d842f4439949e6bad5
Julian Kimmings <35488092+kimmiju@users.noreply.github.com>
2025-08-04 10:25:28 +0100

    Feat: FUT-532 Filter cases based on authenticated user's roles (#170)
    * stub auth utility to get authenticated user roles
    
    * update filter for workflow query
    
    * fetch workflows based on user roles
    
    - filter out any cases that do not match the users roles in case->workflow

--- src/cases/use-cases/find-cases.use-case.js
+++ src/cases/use-cases/find-cases.use-case.js
@@ -1,35 +1,77 @@
+import { getAuthenticatedUserRoles } from "../../common/auth.js";
 import { findUsersUseCase } from "../../users/use-cases/find-users.use-case.js";
 import { findAll } from "../repositories/case.repository.js";
 import { findWorkflowsUseCase } from "./find-workflows.use-case.js";
 
+export const createUserRolesFilter = (userRoles, extrafilters = {}) => {
+  // Checks if all roles are in userRoles. Allows the workflow roles to be empty.
+  const allOf = {
+    $or: [
+      { $eq: [{ $ifNull: ["$requiredRoles.allOf", []] }, []] }, // allow empty roles on workflow
+      { $setIsSubset: ["$requiredRoles.allOf", userRoles] },
+    ],
+  };
+
+  // Checks any (more than one) are in userRoles. Allows the workflow roles to be empty though.
+  const anyOf = {
+    $or: [
+      {
+        $eq: [{ $ifNull: ["$requiredRoles.anyOf", []] }, []], // allow empty roles on workflow
+      },
+      {
+        $gt: [
+          {
+            $size: { $setIntersection: ["$requiredRoles.anyOf", userRoles] },
+          },
+          0,
+        ],
+      },
+    ],
+  };
+
+  return {
+    $expr: {
+      $and: [allOf, anyOf, extrafilters],
+    },
+  };
+};
+
 export const findCasesUseCase = async () => {
+  const userRoles = Object.keys(getAuthenticatedUserRoles());
   const cases = await findAll();
 
   const assignedUserIds = cases.map((c) => c.assignedUser?.id).filter(Boolean);
   const workflowCodes = cases.map((c) => c.workflowCode);
 
+  const workflowFilter = createUserRolesFilter(userRoles, {
+    codes: workflowCodes,
+  });
+
   const [users, workflows] = await Promise.all([
     findUsersUseCase({
       ids: assignedUserIds,
     }),
-    findWorkflowsUseCase({
-      codes: workflowCodes,
-    }),
+    findWorkflowsUseCase(workflowFilter),
   ]);
 
-  for (const kase of cases) {
-    const assignedUser = users.find((u) => u.id === kase.assignedUser?.id);
-
-    if (assignedUser) {
-      kase.assignedUser.name = assignedUser.name;
-    }
-
+  // Remove any cases that the user does not have access to
+  const casesFiltered = cases.reduce((acc, kase) => {
     const workflow = workflows.find((w) => w.code === kase.workflowCode);
 
+    // We only add cases if there's a workflow that was filtered above.
     if (workflow) {
       kase.requiredRoles = workflow.requiredRoles;
-    }
-  }
 
-  return cases;
+      // Only then do we look up the assigned user.
+      const assignedUser = users.find((u) => u.id === kase.assignedUser?.id);
+
+      if (assignedUser) {
+        kase.assignedUser.name = assignedUser.name;
+      }
+      acc.push(kase);
+    }
+    return acc;
+  }, []);
+
+  return casesFiltered;
 };
