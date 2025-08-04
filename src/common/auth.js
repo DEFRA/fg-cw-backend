fbe35f8f6732a23b79ff620fd677bfc300392797
Julian Kimmings <35488092+kimmiju@users.noreply.github.com>
2025-08-04 10:25:28 +0100

    Feat: FUT-532 Filter cases based on authenticated user's roles (#170)
    * stub auth utility to get authenticated user roles
    
    * update filter for workflow query
    
    * fetch workflows based on user roles
    
    - filter out any cases that do not match the users roles in case->workflow

---
+++ src/common/auth.js
@@ -0,0 +1,9 @@
+// TODO: this is a stub. Replace when auth has been implemented.
+export const getAuthenticatedUserRoles = () => ({
+  ROLE_RPA: {},
+  ROLE_RPA_ADMIN: {},
+  ROLE_FLYING_PIGS: {},
+  ROLE_1: {},
+  ROLE_2: {},
+  ROLE_3: {},
+});
