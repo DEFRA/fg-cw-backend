529c779869204577a3ade2210aa684889acaf530
Donatas Bacius <bacius@gmail.com>
2025-08-13 14:43:47 +0100

    Enable Squid proxy use accross the service
    https://eaflood.atlassian.net/browse/FGP-215

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,5 +6,8 @@ env | grep "^TRUSTSTORE_" | cut -d'=' -f2- | base64 -d > /tmp/certs.pem
 
 export NODE_EXTRA_CA_CERTS="/tmp/certs.pem"
 
-node .
+# Use HTTP_PROXY and HTTPS_PROXY environment variables
+export GLOBAL_AGENT_ENVIRONMENT_VARIABLE_NAMESPACE=
+
+node --import "global-agent/bootstrap.js" .
 
