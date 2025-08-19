423e7b817a1fb0a6fce85fbedb83224b6f924b14
julian <juliankimmings@gmail.com>
2025-08-19 14:05:25 +0100

    Add migrations for notes on tasks (pmf)
    - update the create case script to allow pmf cases

---
+++ migrations/20250819122655-update-pmf-workflow-tasks.js
@@ -0,0 +1,136 @@
+export const up = async (db) => {
+  await db.collection("workflows").updateOne(
+    { code: "pigs-might-fly" },
+    {
+      $set: {
+        // Update tasks - no note required
+        "stages.0.taskGroups.0.tasks.0.hasNote": "none",
+        "stages.1.taskGroups.0.tasks": [
+          {
+            id: "check-application-and-documents",
+            title: "Check application and documents",
+            type: "boolean",
+            hasNote: "none",
+          },
+          {
+            id: "check-find-farm-and-land-payment-data",
+            title: "Check on Find farm and land payment data",
+            type: "boolean",
+            hasNote: "none",
+          },
+          {
+            id: "check-rps-dual-funding",
+            title: "Check on RPS (Dual Funding)",
+            type: "boolean",
+            hasNote: "none",
+          },
+        ],
+        "stages.1.taskGroups.1.tasks": [
+          {
+            id: "confirm-farm-has-cph",
+            title: "Confirm farm has a CPH",
+            type: "boolean",
+            hasNote: "none",
+          },
+          {
+            id: "confirm-apha-registration",
+            title: "Confirm APHA registration",
+            type: "boolean",
+            hasNote: "none",
+          },
+        ],
+        "stages.1.taskGroups.2": {
+          id: "review-available-area-checks",
+          title: "Review available area checks",
+          tasks: [
+            {
+              id: "so3757-3159",
+              title: "SFI available area check",
+              type: "boolean",
+              hasNote: "required",
+            },
+            {
+              id: "so3757-3164",
+              title: "SFI available area check",
+              type: "boolean",
+              hasNote: "required",
+            },
+            {
+              id: "so3757-confirm-area-check",
+              title: "Confirm available area check",
+              type: "boolean",
+              hasNote: "optional",
+            },
+          ],
+        },
+        "stages.1.taskGroups.3": {
+          id: "review-intersecting-data-layers",
+          title: "Review intersecting data layers",
+          tasks: [
+            {
+              id: "so3756-3059",
+              title: "SFI intersecting layers check",
+              type: "boolean",
+              hasNote: "required",
+            },
+            {
+              id: "so3756-3064",
+              title: "SFI intersecting layers check",
+              type: "boolean",
+              hasNote: "optional",
+            },
+            {
+              id: "so3757-confirm-area-check",
+              title: "Confirm available area check",
+              type: "boolean",
+              hasNote: "optional",
+            },
+          ],
+        },
+      },
+    },
+  );
+};
+
+export const down = async (db) => {
+  await db.collection("workflows").updateOne(
+    { code: "pigs-might-fly" },
+    {
+      $set: {
+        // Revert task note
+        "stages.0.taskGroups.0.tasks.0.hasNote": null,
+        "stages.1.taskGroups.0.tasks": [
+          {
+            id: "check-application-and-documents",
+            title: "Check application and documents",
+            type: "boolean",
+          },
+          {
+            id: "check-find-farm-and-land-payment-data",
+            title: "Check on Find farm and land payment data",
+            type: "boolean",
+          },
+          {
+            id: "check-rps-dual-funding",
+            title: "Check on RPS (Dual Funding)",
+            type: "boolean",
+          },
+        ],
+        "stages.1.taskGroups.1.tasks": [
+          {
+            id: "confirm-farm-has-cph",
+            title: "Confirm farm has a CPH",
+            type: "boolean",
+          },
+          {
+            id: "confirm-apha-registration",
+            title: "Confirm APHA registration",
+            type: "boolean",
+          },
+        ],
+        "stages.1.taskGroups.2": null,
+        "stages.1.taskGroups.3": null,
+      },
+    },
+  );
+};
