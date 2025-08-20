9bf22837ed4fec9bbfce0c7eae73937208c8bb4a
Donatas Bacius <bacius@gmail.com>
2025-08-20 15:57:51 +0100

    Using Entra ID stub locally
    https://eaflood.atlassian.net/browse/FGP-215
    
    - CW-BE will use it to verify tokens
    - CW-BE integration tests can get a token to impersonate a user
    - CW-FE will use the same instance to log in

---
+++ test/users/find-secret.test.js
@@ -0,0 +1,31 @@
+import { env } from "node:process";
+import { describe, expect, it } from "vitest";
+import { wreck } from "../helpers/wreck.js";
+
+describe("GET /secret", () => {
+  it("finds secrets", async () => {
+    const tokenResponse = await wreck.post(env.OIDC_SIGN_TOKEN_ENDPOINT, {
+      payload: {
+        clientId: "client1",
+        username: "admin@t.gov.uk",
+      },
+    });
+
+    const response = await wreck.get("/secret", {
+      headers: {
+        Authorization: `Bearer ${tokenResponse.payload.access_token}`,
+      },
+    });
+
+    expect(response).toEqual({
+      res: expect.objectContaining({
+        statusCode: 200,
+      }),
+      payload: {
+        idpId: "9f6b80d3-99d3-42dc-ac42-b184595b1ef1",
+        idpRoles: ["FCP.Casework.Admin"],
+        name: "Test Admin",
+      },
+    });
+  });
+});
