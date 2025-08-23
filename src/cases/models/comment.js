7518e8cc79f96a4d0a49db3c1f8c00d4fe0bc370
Justin Ramel <justin.ramel@gmail.com>
2025-08-23 08:06:37 +0100

    revert comment changes

--- src/cases/models/comment.js
+++ src/cases/models/comment.js
@@ -1,3 +1,4 @@
+import Boom from "@hapi/boom";
 import Joi from "joi";
 import { ObjectId } from "mongodb";
 import {
@@ -7,19 +8,28 @@ import {
 import { timelineEventTypeSchema } from "../schemas/cases/timeline/event-type.schema.js";
 import { idSchema } from "../schemas/id.schema.js";
 import { EventEnums } from "./event-enums.js";
-import { validateModel } from "./validate-model.js";
 
 export class Comment {
-  static schema = Joi.object({
+  static validationSchema = Joi.object({
     ref: idSchema,
     type: timelineEventTypeSchema.required(),
     text: Joi.string().required(),
     createdBy: Joi.string().required(),
     createdAt: Joi.string().isoDate(),
-  }).label("CommentSchema");
+  });
 
   constructor(props) {
-    const value = validateModel(props, Comment.schema);
+    const { error, value } = Comment.validationSchema.validate(props, {
+      stripUnknown: true,
+      abortEarly: false,
+    });
+
+    if (error) {
+      throw Boom.badRequest(
+        `Invalid Comment: ${error.details.map((d) => d.message).join(", ")}`,
+      );
+    }
+
     this.ref = value.ref || new ObjectId().toHexString();
     this.type = value.type;
     this.text = value.text;
