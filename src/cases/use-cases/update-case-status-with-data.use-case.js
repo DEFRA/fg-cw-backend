6d805dd7b691aba2671da2d844c7f9dff7357a4b
julian <juliankimmings@gmail.com>
2025-09-08 11:21:39 +0100

    move update with data

---
+++ src/cases/use-cases/update-case-status-with-data.use-case.js
@@ -0,0 +1,27 @@
+import Boom from "@hapi/boom";
+import { getAuthenticatedUser } from "../../common/auth.js";
+import {
+  findByCaseRefAndWorkflowCode,
+  update,
+} from "../repositories/case.repository.js";
+
+export const updateCaseStatusWithDataUseCase = async ({
+  caseRef,
+  workflowCode,
+  newStatus,
+  supplementaryData,
+}) => {
+  const kase = await findByCaseRefAndWorkflowCode(caseRef, workflowCode);
+
+  if (!kase) {
+    throw Boom.notFound(`Case with ref "${caseRef}" not found`);
+  }
+  const createdBy = getAuthenticatedUser().id;
+
+  kase.updateStatus(newStatus, createdBy);
+  kase.addDataToStage(supplementaryData);
+
+  await update(kase);
+
+  return kase;
+};
