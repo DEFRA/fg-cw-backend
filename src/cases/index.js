38bd0d8521b8efc8afe6918cc2128ab0ed4e0361
Justin Ramel <justin.ramel@gmail.com>
2025-09-16 00:36:26 +0100

    Update `frps` and `pmf` workflows to new format and add environment context to migrations

--- src/cases/index.js
+++ src/cases/index.js
@@ -5,6 +5,7 @@ import { db, mongoClient } from "../common/mongo-client.js";
 import { createNewCaseSubscriber } from "./subscribers/create-new-case.subscriber.js";
 import { createUpdateStatusAgreementConsumer } from "./subscribers/update-case-status-agreement.subscriber.js";
 
+import { config } from "../common/config.js";
 import { addNoteToCaseRoute } from "./routes/add-note-to-case.route.js";
 import { assignUserToCaseRoute } from "./routes/assign-user-to-case.route.js";
 import { createWorkflowRoute } from "./routes/create-workflow.route.js";
@@ -20,7 +21,15 @@ export const cases = {
   name: "cases",
   async register(server) {
     logger.info("Running migrations");
-    const migrated = await up(db, mongoClient);
+
+    const environment = config.get("cdpEnvironment");
+
+    const context = {
+      environment,
+    };
+
+    const migrated = await up(db, mongoClient, context);
+
     migrated.forEach((fileName) => logger.info(`Migrated: ${fileName}`));
     logger.info("Finished running migrations");
 
