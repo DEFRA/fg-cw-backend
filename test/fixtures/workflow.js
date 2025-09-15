0ed559bd81a9a37505f49a9435f8139803d84b99
Justin Ramel <justin.ramel@gmail.com>
2025-09-15 20:35:08 +0100

    fix workflow schema tests

--- test/fixtures/workflow.js
+++ test/fixtures/workflow.js
@@ -5,40 +5,241 @@ export const workflowData1 = {
       details: {
         banner: {
           title: {
-            ref: "$.payload.businessName",
+            text: "$.payload.businessName",
             type: "string",
           },
           summary: {
             sbi: {
               label: "SBI",
-              ref: "$.payload.identifiers.sbi",
+              text: "$.payload.identifiers.sbi",
               type: "string",
             },
-            clientReference: {
-              label: "Client Reference",
-              ref: "$.payload.clientRef",
+            reference: {
+              label: "Reference",
+              text: "$.caseRef",
               type: "string",
+            },
+            scheme: {
+              label: "Scheme",
+              text: "$.payload.answers.scheme",
+              type: "string",
+            },
+            createdAt: {
+              label: "Created At",
+              text: "$.payload.createdAt",
+              type: "date",
+              format: "formatDate",
             },
           },
         },
         tabs: {
-          caseDetails: {
-            title: "Application",
-            sections: [
+          "case-details": {
+            link: {
+              id: "case-details",
+              href: {
+                urlTemplate: "/cases/{caseId}/case-details",
+                params: {
+                  caseId: "$._id",
+                },
+              },
+              text: "Case Details",
+              index: 1,
+            },
+            content: [
               {
-                title: "Details",
-                type: "object",
-                component: "list",
-                fields: [
+                id: "title",
+                component: "heading",
+                text: "Application",
+                level: 2,
+                classes: "govuk-!-margin-top-8 govuk-!-margin-bottom-1",
+              },
+              {
+                id: "submittedDate",
+                component: "container",
+                classes: "govuk-body",
+                elements: [
                   {
-                    ref: "$.payload.answers.scheme",
+                    text: "submitted:",
+                    classes: "govuk-!-font-weight-regular",
+                  },
+                  {
+                    text: "$.payload.submittedAt",
+                    classes: "govuk-!-font-weight-bold",
+                    format: "formatDate",
+                  },
+                ],
+              },
+              {
+                id: "answers",
+                component: "list",
+                title: "Answers",
+                type: "object",
+                rows: [
+                  {
+                    text: "$.payload.answers.scheme",
                     type: "string",
                     label: "Scheme",
                   },
                   {
-                    ref: "$.payload.answers.year",
+                    text: "$.payload.answers.year",
                     type: "number",
                     label: "Year",
+                  },
+                  {
+                    text: "$.payload.answers.hasCheckedLandIsUpToDate",
+                    type: "boolean",
+                    label: "Has checked land is up to date?",
+                    format: "yesNo",
+                  },
+                  {
+                    text: "$.payload.answers.agreementName",
+                    type: "string",
+                    label: "Agreement Name",
+                  },
+                ],
+              },
+              {
+                id: "actions",
+                component: "table",
+                rowsRef: "$.payload.answers.actionApplications[*]",
+                title: "Selected actions for land parcels",
+                type: "array",
+                firstCellIsHeader: true,
+                rows: [
+                  {
+                    text: "@.sheetId",
+                    type: "string",
+                    label: "Sheet Id",
+                  },
+                  {
+                    text: "@.parcelId",
+                    type: "string",
+                    label: "Parcel Id",
+                  },
+                  {
+                    text: "@.code",
+                    type: "string",
+                    label: "Code",
+                  },
+                  {
+                    id: "appliedFor",
+                    component: "container",
+                    label: "Applied For",
+                    elements: [
+                      {
+                        text: "@.appliedFor.quantity",
+                        type: "number",
+                        format: "fixed(4)",
+                      },
+                      {
+                        text: "@.appliedFor.unit",
+                        type: "string",
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          agreements: {
+            renderIf: "$.agreements[0]",
+            link: {
+              id: "agreements",
+              href: {
+                urlTemplate: "/cases/{caseId}/agreements",
+                params: {
+                  caseId: "$._id",
+                },
+              },
+              text: "Agreements",
+            },
+            content: [
+              {
+                id: "title",
+                component: "heading",
+                text: "Case grant funding agreement",
+                level: 2,
+                classes: "govuk-!-margin-top-8",
+              },
+              {
+                id: "subtitle",
+                component: "heading",
+                text: "Check the grant funding agreement system to see the final terms of the agreement.",
+                level: 3,
+                classes: "govuk-inset-text",
+              },
+              {
+                id: "agreementName",
+                component: "container",
+                classes: "govuk-body",
+                elements: [
+                  {
+                    text: "Agreement name:",
+                    classes: "govuk-!-font-weight-bold",
+                  },
+                  {
+                    text: "[agreement name]",
+                  },
+                ],
+              },
+              {
+                type: "array",
+                component: "table",
+                rowsRef: "$.agreements[*]",
+                rows: [
+                  {
+                    label: "Reference",
+                    text: "@.agreementRef",
+                  },
+                  {
+                    label: "Date",
+                    component: "date",
+                    text: "@.createdAt",
+                  },
+                  {
+                    id: "internal",
+                    component: "container",
+                    label: "View",
+                    elements: [
+                      {
+                        label: "Internal",
+                        component: "url",
+                        text: "Internal",
+                        href: {
+                          urlTemplate:
+                            "$.definitions.agreementsService.internalUrlTemplate",
+                          params: {
+                            agreementRef: "@.agreementRef",
+                          },
+                        },
+                        target: "_blank",
+                        rel: "noopener",
+                        classes: "govuk-!-margin-right-6",
+                      },
+                      {
+                        label: "External",
+                        component: "copyToClipboard",
+                        text: {
+                          urlTemplate:
+                            "$.definitions.agreementsService.externalUrlTemplate",
+                          params: {
+                            agreementRef: "@.agreementRef",
+                          },
+                        },
+                        buttonText: "Copy external",
+                        feedbackText: "Copied to clipboard",
+                      },
+                    ],
+                  },
+                  {
+                    label: "Status",
+                    component: "status",
+                    text: "@.agreementStatus",
+                    classesMap: {
+                      OFFERED: "govuk-tag--yellow",
+                      ACCEPTED: "govuk-tag--blue",
+                      WITHDRAWN: "govuk-tag--grey",
+                    },
                   },
                 ],
               },
@@ -48,6 +249,7 @@ export const workflowData1 = {
       },
     },
   },
+
   stages: [
     {
       id: "application-receipt",
