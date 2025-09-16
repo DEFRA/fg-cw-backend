4e8804aea99ec029ce0304f1b4d5b3d2f6380035
Donatas Bacius <bacius@gmail.com>
2025-09-16 16:51:13 +0100

    Swap to use new queue for CreateCaseCommand
    https://eaflood.atlassian.net/browse/FGP-526

--- src/cases/subscribers/create-new-case.subscriber.js
+++ src/cases/subscribers/create-new-case.subscriber.js
@@ -4,7 +4,7 @@ import { SqsSubscriber } from "../../common/sqs-subscriber.js";
 import { createCaseUseCase } from "../use-cases/create-case.use-case.js";
 
 export const createNewCaseSubscriber = new SqsSubscriber({
-  queueUrl: config.get("aws.createNewCaseSqsUrl"),
+  queueUrl: config.get("aws.sqs.createNewCaseUrl"),
   async onMessage(message) {
     const { data } = message;
 
