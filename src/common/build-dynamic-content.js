532ae7493c42ba2aea87034d51317db2a0a89802
Justin Ramel <justin.ramel@gmail.com>
2025-09-24 10:36:00 +0100

    Extract `isDateString` to dedicated `date-helpers.js`, add tests

--- src/common/build-dynamic-content.js
+++ src/common/build-dynamic-content.js
@@ -1,3 +1,5 @@
+import { isDateString } from "./date-helpers.js";
+
 const EXCLUDE_KEYS = [
   "clientRef",
   "code",
@@ -115,21 +117,6 @@ const createCustomTitle = (objKey, parentKey) => {
   return isNumericKey && hasMeaningfulParent
     ? `${camelCaseToTitleCase(parentKey)} ${objKey}`
     : null;
-};
-
-const isDateString = (value) => {
-  if (typeof value !== "string") {
-    return false;
-  }
-
-  const isoDateRegex = /^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2}(\.\d{3})?Z?)?$/;
-
-  if (!isoDateRegex.test(value)) {
-    return false;
-  }
-
-  const date = new Date(value);
-  return !isNaN(date.getTime());
 };
 
 const isObject = (value) => {
