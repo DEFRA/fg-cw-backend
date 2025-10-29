ee207ce716ddda642e8287b6ac4810d27b1c3c36
julian <juliankimmings@gmail.com>
2025-10-29 11:20:03 +0000

    update env.example
    adds env vars for use with grants-core

--- src/common/mongo-client.js
+++ src/common/mongo-client.js
@@ -2,9 +2,13 @@ import { MongoClient } from "mongodb";
 import tls from "node:tls";
 import { config } from "./config.js";
 
+export const getReadPreference = () => {
+  return process.env.NODE_ENV === "production" ? "secondary" : "primary";
+};
+
 export const mongoClient = new MongoClient(config.get("mongo.uri"), {
   retryWrites: false,
-  readPreference: "secondary",
+  readPreference: getReadPreference(),
   secureContext: tls.createSecureContext(),
 });
 
