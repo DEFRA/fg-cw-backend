3dac79dc75211c0cdb35cd6277de85977f0e5f21
julian <juliankimmings@gmail.com>
2025-10-29 11:59:12 +0000

    revert mongo client

--- src/common/mongo-client.js
+++ src/common/mongo-client.js
@@ -2,13 +2,9 @@ import { MongoClient } from "mongodb";
 import tls from "node:tls";
 import { config } from "./config.js";
 
-export const getReadPreference = () => {
-  return process.env.NODE_ENV === "production" ? "secondary" : "primary";
-};
-
 export const mongoClient = new MongoClient(config.get("mongo.uri"), {
   retryWrites: false,
-  readPreference: getReadPreference(),
+  readPreference: "secondary",
   secureContext: tls.createSecureContext(),
 });
 
