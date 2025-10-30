67ceb0ed7b59fdd95041f8c219cbd566a5393458
julian <juliankimmings@gmail.com>
2025-10-30 11:40:29 +0000

    infrastructure for replicasets

--- src/common/config.js
+++ src/common/config.js
@@ -89,7 +89,7 @@ export const config = convict({
     uri: {
       doc: "URI for mongodb",
       format: String,
-      default: "mongodb://127.0.0.1:27017/",
+      default: "mongodb://127.0.0.1:27017/?replicaSet=mongoRepl",
       env: "MONGO_URI",
     },
     databaseName: {
