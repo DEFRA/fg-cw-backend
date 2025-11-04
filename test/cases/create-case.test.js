e31b48835ee1528b7637a8ec5599f04714a69285
julian <juliankimmings@gmail.com>
2025-11-04 08:59:07 +0000

    update create case test

--- test/cases/create-case.test.js
+++ test/cases/create-case.test.js
@@ -1,4 +1,5 @@
 import { MongoClient, ObjectId } from "mongodb";
+import { randomUUID } from "node:crypto";
 import { env } from "node:process";
 import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
 import { caseData3Document } from "../fixtures/case.js";
@@ -20,6 +21,11 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await client.close(true);
+});
+
+beforeEach(async () => {
+  await cases.deleteMany({});
+  await inbox.deleteMany({});
 });
 
 describe("On CreateNewCase event", () => {
@@ -54,10 +60,13 @@ describe("On CreateNewCase event", () => {
     expected[0].phases[0].stages[0].outcome = null;
     expected[0].phases[0].stages[1].outcome = null;
 
+    const messageId = randomUUID();
+    createCaseEvent3.id = messageId;
     await sendMessage(env.CW__SQS__CREATE_NEW_CASE_URL, createCaseEvent3);
 
     const documents = await waitForDocuments(inbox, 3, {
       target: env.CW__SQS__CREATE_NEW_CASE_URL,
+      messageId,
     });
 
     expect(documents).toHaveLength(1);
