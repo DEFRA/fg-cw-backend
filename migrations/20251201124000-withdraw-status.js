0089a552071eaae13af0fcbf94de5d5820e93a39
julian <juliankimmings@gmail.com>
2025-12-04 12:19:41 +0000

    update migration to use transactions

--- migrations/20251201124000-withdraw-status.js
+++ migrations/20251201124000-withdraw-status.js
@@ -1,6 +1,7 @@
-const query = { code: "frps-private-beta" };
+import { withTransaction } from "../src/common/with-transaction.js";
 
 export const up = async (db) => {
+  const query = { code: "frps-private-beta" };
   const withdrawRequestedTransition = {
     targetPosition: "PRE_AWARD:REVIEW_APPLICATION:WITHDRAW_REQUESTED",
     action: {
@@ -8,8 +9,8 @@ export const up = async (db) => {
       name: "Withdraw",
       checkTasks: false,
       comment: {
-        label: "Note",
-        helpText: "All notes will be saved for auditing purposes",
+        label: "Explain this decision",
+        helpText: "You must include an explanation for auditing purposes.",
         mandatory: true,
       },
     },
@@ -42,8 +43,8 @@ export const up = async (db) => {
           name: "Reinstate Application",
           checkTasks: false,
           comment: {
-            label: "Note",
-            helpText: "All notes will be saved for auditing purposes",
+            label: "Explain this decision",
+            helpText: "You must include an explanation for auditing purposes.",
             mandatory: true,
           },
         },
@@ -52,259 +53,308 @@ export const up = async (db) => {
     ],
   };
 
-  const collection = db.collection("workflows");
+  await withTransaction(async (session) => {
+    const collection = db.collection("workflows");
 
-  // add checkTasks to all transitions - default to true
-  collection.updateMany(query, [
-    {
-      $set: {
-        phases: {
-          $map: {
-            input: "$phases",
-            as: "phase",
-            in: {
-              $mergeObjects: [
-                "$$phase",
-                {
-                  stages: {
-                    $map: {
-                      input: "$$phase.stages",
-                      as: "stage",
-                      in: {
-                        $mergeObjects: [
-                          "$$stage",
-                          {
-                            statuses: {
-                              $map: {
-                                input: "$$stage.statuses",
-                                as: "status",
-                                in: {
-                                  $mergeObjects: [
-                                    "$$status",
-                                    {
-                                      transitions: {
-                                        $map: {
-                                          input: "$$status.transitions",
-                                          as: "transition",
-                                          in: {
-                                            $mergeObjects: [
-                                              "$$transition",
-                                              { checkTasks: true },
-                                            ],
+    // add checkTasks to all transitions - default to true
+    await collection.updateMany(
+      query,
+      [
+        {
+          $set: {
+            phases: {
+              $map: {
+                input: "$phases",
+                as: "phase",
+                in: {
+                  $mergeObjects: [
+                    "$$phase",
+                    {
+                      stages: {
+                        $map: {
+                          input: "$$phase.stages",
+                          as: "stage",
+                          in: {
+                            $mergeObjects: [
+                              "$$stage",
+                              {
+                                statuses: {
+                                  $map: {
+                                    input: "$$stage.statuses",
+                                    as: "status",
+                                    in: {
+                                      $mergeObjects: [
+                                        "$$status",
+                                        {
+                                          transitions: {
+                                            $map: {
+                                              input: "$$status.transitions",
+                                              as: "transition",
+                                              in: {
+                                                $mergeObjects: [
+                                                  "$$transition",
+                                                  { checkTasks: true },
+                                                ],
+                                              },
+                                            },
                                           },
                                         },
-                                      },
+                                      ],
                                     },
-                                  ],
+                                  },
                                 },
                               },
-                            },
+                            ],
                           },
-                        ],
+                        },
                       },
                     },
-                  },
+                  ],
                 },
-              ],
+              },
             },
           },
         },
-      },
-    },
-  ]);
+      ],
+      { session },
+    );
 
-  // add additional statuses and transitions and turn off checking tasks for application withdrawal
-  // PRE_AWARD:REVIEW_APPLICATION
-  // :IN_REVIEW
-  collection.updateOne(
-    query,
-    {
-      $push: {
-        "phases.$[phase].stages.$[stage].statuses": {
-          $each: [withdrawRequestedStatus, applicationWithdrawnStatus],
+    // PRE_AWARD:REVIEW_APPLICATION
+    // :IN_REVIEW
+    await collection.updateOne(
+      query,
+      {
+        $push: {
+          "phases.$[phase].stages.$[stage].statuses": {
+            $each: [withdrawRequestedStatus, applicationWithdrawnStatus],
+          },
         },
       },
-    },
-    {
-      arrayFilters: [
-        { "phase.code": "PRE_AWARD" },
-        { "stage.code": "REVIEW_APPLICATION" },
-      ],
-    },
-  );
+      {
+        arrayFilters: [
+          { "phase.code": "PRE_AWARD" },
+          { "stage.code": "REVIEW_APPLICATION" },
+        ],
+        session,
+      },
+    );
 
-  collection.updateOne(
-    query,
-    {
-      $push: {
-        "phases.$[phase].stages.$[stage].statuses.$[status].transitions":
-          withdrawRequestedTransition,
+    await collection.updateOne(
+      query,
+      {
+        $push: {
+          "phases.$[phase].stages.$[stage].statuses.$[status].transitions":
+            withdrawRequestedTransition,
+        },
       },
-    },
-    {
-      arrayFilters: [
-        { "phase.code": "PRE_AWARD" },
-        { "stage.code": "REVIEW_APPLICATION" },
-        { "status.code": "IN_REVIEW" },
-      ],
-    },
-  );
+      {
+        arrayFilters: [
+          { "phase.code": "PRE_AWARD" },
+          { "stage.code": "REVIEW_APPLICATION" },
+          { "status.code": "IN_REVIEW" },
+        ],
+        session,
+      },
+    );
 
-  collection.updateOne(
-    query,
-    {
-      $set: {
-        "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition].checkTasks": false,
-      },
-    },
-    {
-      arrayFilters: [
-        { "phase.code": "PRE_AWARD" },
-        { "stage.code": "REVIEW_APPLICATION" },
-        { "status.code": "IN_REVIEW" },
-        {
-          "transition.targetPosition":
-            "PRE_AWARD:REVIEW_APPLICATION:WITHDRAW_REQUESTED",
+    // :AGREEMENT_GENERATING
+    await collection.updateOne(
+      query,
+      {
+        $push: {
+          "phases.$[phase].stages.$[stage].statuses.$[status].transitions":
+            withdrawRequestedTransition,
         },
-      ],
-    },
-  );
+      },
+      {
+        arrayFilters: [
+          { "phase.code": "PRE_AWARD" },
+          { "stage.code": "REVIEW_APPLICATION" },
+          { "status.code": "AGREEMENT_GENERATING" },
+        ],
+        session,
+      },
+    );
 
-  // :WITHDRAW_REQUESTED
-  collection.updateOne(
-    query,
-    {
-      $set: {
-        "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition].checkTasks": false,
-      },
-    },
-    {
-      arrayFilters: [
-        { "phase.code": "PRE_AWARD" },
-        { "stage.code": "REVIEW_APPLICATION" },
-        { "status.code": "WITHDRAW_REQUESTED" },
-        {
-          "transition.targetPosition":
-            "PRE_AWARD:REVIEW_APPLICATION:APPLICATION_WITHDRAWN",
+    // PRE_AWARD:REVIEW_OFFER
+    // :AGREEMENT_DRAFTED
+    await collection.updateOne(
+      query,
+      {
+        $push: {
+          "phases.$[phase].stages.$[stage].statuses.$[status].transitions":
+            withdrawRequestedTransition,
         },
-      ],
-    },
-  );
-  // :APPLICATION_WITHDRAWN
-  collection.updateOne(
-    query,
-    {
-      $set: {
-        "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition].checkTasks": false,
       },
-    },
-    {
-      arrayFilters: [
-        { "phase.code": "PRE_AWARD" },
-        { "stage.code": "REVIEW_APPLICATION" },
-        { "status.code": "APPLICATION_WITHDRAWN" },
-        {
-          "transition.targetPosition": "PRE_AWARD:REVIEW_APPLICATION:IN_REVIEW",
-        },
-      ],
-    },
-  );
+      {
+        arrayFilters: [
+          { "phase.code": "PRE_AWARD" },
+          { "stage.code": "REVIEW_OFFER" },
+          { "status.code": "AGREEMENT_DRAFTED" },
+        ],
+        session,
+      },
+    );
 
-  // PRE_AWARD:REVIEW_OFFER
-  // :AGREEMENT_DRAFTED
-  collection.updateOne(
-    query,
-    {
-      $push: {
-        "phases.$[phase].stages.$[stage].statuses.$[status].transitions":
-          withdrawRequestedTransition,
-      },
-    },
-    {
-      arrayFilters: [
-        { "phase.code": "PRE_AWARD" },
-        { "stage.code": "REVIEW_OFFER" },
-        { "status.code": "AGREEMENT_DRAFTED" },
-      ],
-    },
-  );
-  collection.updateOne(
-    query,
-    {
-      $set: {
-        "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition].checkTasks": false,
-      },
-    },
-    {
-      arrayFilters: [
-        { "phase.code": "PRE_AWARD" },
-        { "stage.code": "REVIEW_OFFER" },
-        { "status.code": "AGREEMENT_DRAFTED" },
-        {
-          "transition.targetPosition":
-            "PRE_AWARD:REVIEW_APPLICATION:WITHDRAW_REQUESTED",
+    // PRE_AWARD:CUSTOMER_AGREEMENT_REVIEW
+    // :AGREEMENT_OFFERED
+    await collection.updateOne(
+      query,
+      {
+        $push: {
+          "phases.$[phase].stages.$[stage].statuses.$[status].transitions":
+            withdrawRequestedTransition,
         },
-      ],
-    },
-  );
-
-  // PRE_AWARD:CUSTOMER_AGREEMENT_REVIEW
-  // :AGREEMENT_OFFERED
-  collection.updateOne(
-    query,
-    {
-      $push: {
-        "phases.$[phase].stages.$[stage].statuses.$[status].transitions":
-          withdrawRequestedTransition,
       },
-    },
-    {
-      arrayFilters: [
-        { "phase.code": "PRE_AWARD" },
-        { "stage.code": "CUSTOMER_AGREEMENT_REVIEW" },
-        { "status.code": "AGREEMENT_OFFERED" },
-      ],
-    },
-  );
-  collection.updateOne(
-    query,
-    {
-      $set: {
-        "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition].checkTasks": false,
+      {
+        arrayFilters: [
+          { "phase.code": "PRE_AWARD" },
+          { "stage.code": "CUSTOMER_AGREEMENT_REVIEW" },
+          { "status.code": "AGREEMENT_OFFERED" },
+        ],
+        session,
       },
-    },
-    {
-      arrayFilters: [
-        { "phase.code": "PRE_AWARD" },
-        { "stage.code": "CUSTOMER_AGREEMENT_REVIEW" },
-        { "status.code": "AGREEMENT_OFFERED" },
-        {
-          "transition.targetPosition":
-            "PRE_AWARD:REVIEW_APPLICATION:WITHDRAW_REQUESTED",
-        },
-      ],
-    },
-  );
+    );
+  });
 };
 
-export const down = (db) => {
+export const down = async (db) => {
+  const query = { code: "frps-private-beta" };
   const collection = db.collection("workflows");
-
-  collection.updateMany(
-    query,
-    {
-      $unset: {
-        "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition].checkTasks":
-          "",
+  await withTransaction(async (session) => {
+    await collection.updateMany(
+      query,
+      {
+        $unset: {
+          "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition].checkTasks":
+            "",
+        },
       },
-    },
-    {
-      arrayFilters: [
-        { phase: { $exists: true } },
-        { stage: { $exists: true } },
-        { status: { $exists: true } },
-        { "transition.checkTasks": { $exists: true } },
-      ],
-    },
-  );
+      {
+        arrayFilters: [
+          { phase: { $exists: true } },
+          { stage: { $exists: true } },
+          { status: { $exists: true } },
+          { "transition.checkTasks": { $exists: true } },
+        ],
+        session,
+      },
+    );
+
+    // PRE_AWARD:REVIEW_APPLICATION
+    // :IN_REVIEW
+    await collection.updateOne(
+      query,
+      {
+        $unset: {
+          "phases.$[phase].stages.$[stage].statuses.$[status]": "",
+        },
+      },
+      {
+        arrayFilters: [
+          { "phase.code": "PRE_AWARD" },
+          { "stage.code": "REVIEW_APPLICATION" },
+          {
+            "status.code": {
+              $in: ["APPLICATION_WITHDRAWN", "WITHDRAW_REQUESTED"],
+            },
+          },
+        ],
+        session,
+      },
+    );
+
+    await collection.updateOne(
+      query,
+      {
+        $unset: {
+          "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition]":
+            "",
+        },
+      },
+      {
+        arrayFilters: [
+          { "phase.code": "PRE_AWARD" },
+          { "stage.code": "REVIEW_APPLICATION" },
+          { "status.code": "IN_REVIEW" },
+          {
+            "transition.targetPosition":
+              "PRE_AWARD:REVIEW_APPLICATION:WITHDRAW_REQUESTED",
+          },
+        ],
+        session,
+      },
+    );
+
+    // :AGREEMENT_GENERATING
+    await collection.updateOne(
+      query,
+      {
+        $unset: {
+          "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition]":
+            "",
+        },
+      },
+      {
+        arrayFilters: [
+          { "phase.code": "PRE_AWARD" },
+          { "stage.code": "REVIEW_APPLICATION" },
+          { "status.code": "AGREEMENT_GENERATING" },
+          {
+            "transition.targetPosition":
+              "PRE_AWARD:REVIEW_APPLICATION:WITHDRAW_REQUESTED",
+          },
+        ],
+        session,
+      },
+    );
+
+    // PRE_AWARD:REVIEW_OFFER
+    // :AGREEMENT_DRAFTED
+    await collection.updateOne(
+      query,
+      {
+        $unset: {
+          "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition]":
+            "",
+        },
+      },
+      {
+        arrayFilters: [
+          { "phase.code": "PRE_AWARD" },
+          { "stage.code": "REVIEW_OFFER" },
+          { "status.code": "AGREEMENT_DRAFTED" },
+          {
+            "transition.targetPosition":
+              "PRE_AWARD:REVIEW_APPLICATION:WITHDRAW_REQUESTED",
+          },
+        ],
+        session,
+      },
+    );
+
+    // PRE_AWARD:CUSTOMER_AGREEMENT_REVIEW
+    // :AGREEMENT_OFFERED
+    await collection.updateOne(
+      query,
+      {
+        $unset: {
+          "phases.$[phase].stages.$[stage].statuses.$[status].transitions.$[transition]":
+            "",
+        },
+      },
+      {
+        arrayFilters: [
+          { "phase.code": "PRE_AWARD" },
+          { "stage.code": "CUSTOMER_AGREEMENT_REVIEW" },
+          { "status.code": "AGREEMENT_OFFERED" },
+          {
+            "transition.targetPosition":
+              "PRE_AWARD:REVIEW_APPLICATION:WITHDRAW_REQUESTED",
+          },
+        ],
+        session,
+      },
+    );
+  });
 };
