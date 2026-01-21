88617ec6030218a7a8c5814172f156ba244991f7
Julian Kimmings <35488092+kimmiju@users.noreply.github.com>
2026-01-21 10:28:53 +0000

    FGP-866: adds async parsing for inbox subscriber (#473)
    * adds async parsing for inbox subscriber
    
    * remove async generator
    
    * update unit tests

--- src/cases/subscribers/inbox.subscriber.test.js
+++ src/cases/subscribers/inbox.subscriber.test.js
@@ -1,3 +1,4 @@
+import { setTimeout } from "node:timers/promises";
 import {
   afterAll,
   afterEach,
@@ -48,7 +49,7 @@ describe("inbox.subscriber", () => {
   });
 
   it("should poll on start()", async () => {
-    claimEvents.mockResolvedValue([new Inbox({})]);
+    claimEvents.mockResolvedValue([Inbox.createMock()]);
     const subscriber = new InboxSubscriber();
     subscriber.start();
     expect(claimEvents).toHaveBeenCalled();
@@ -99,7 +100,11 @@ describe("inbox.subscriber", () => {
   });
 
   it("should stop polling after stop()", async () => {
-    claimEvents.mockResolvedValue([new Inbox({})]);
+    claimEvents.mockResolvedValue([
+      Inbox.createMock({
+        event: { time: new Date().toISOString() },
+      }),
+    ]);
     const subscriber = new InboxSubscriber();
     subscriber.start();
     expect(claimEvents).toHaveBeenCalledTimes(1);
@@ -110,6 +115,33 @@ describe("inbox.subscriber", () => {
   });
 
   describe("processEvents", () => {
+    it("should process events in correct order", async () => {
+      const events = [
+        Inbox.createMock({
+          _id: "1",
+          event: { time: new Date(Date.now()).toISOString() },
+        }),
+        Inbox.createMock({
+          _id: "2",
+          event: { time: new Date(Date.now()).toISOString() },
+        }),
+      ];
+
+      claimEvents.mockResolvedValue(events);
+      const subscriber = new InboxSubscriber();
+      const spy1 = vi
+        .spyOn(subscriber, "handleEvent")
+        .mockImplementationOnce(async () => {
+          return setTimeout(500);
+        })
+        .mockImplementationOnce(async () => setTimeout(500));
+      await subscriber.processEvents(events);
+      expect(spy1).toHaveBeenCalledTimes(2);
+
+      expect(subscriber.handleEvent.mock.calls[0][0]).toEqual(events[0]);
+      expect(subscriber.handleEvent.mock.calls[1][0]).toEqual(events[1]);
+    });
+
     it("should use use-cases if mapped", async () => {
       const mockEventData = {
         foo: "barr",
