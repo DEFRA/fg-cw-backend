9aafe71ca96560f94b395dc614bb4e403895d6fc
Justin Ramel <justin.ramel@esynergy.co.uk>
2026-01-29 10:24:25 +0000

    update roles route to require admin role (#486)
    * update roles route to require admin role
    
    * fix integration test, now requires admin user
    
    * return roles sorted by code

--- src/users/routes/find-roles.route.test.js
+++ src/users/routes/find-roles.route.test.js
@@ -1,6 +1,9 @@
+import Boom from "@hapi/boom";
 import hapi from "@hapi/hapi";
 import { afterAll, beforeAll, describe, expect, it, vi } from "vitest";
+
 import { Role } from "../models/role.js";
+import { User } from "../models/user.js";
 import { findRolesResponseSchema } from "../schemas/responses/find-roles-response.schema.js";
 import { findRolesUseCase } from "../use-cases/find-roles.use-case.js";
 import { findRolesRoute } from "./find-roles.route.js";
@@ -20,20 +23,47 @@ describe("findRolesRoute", () => {
     await server.stop();
   });
 
-  it("returns all roles", async () => {
+  it("returns 403 when user is not admin", async () => {
+    findRolesUseCase.mockRejectedValue(Boom.forbidden("Forbidden"));
+
+    const { statusCode } = await server.inject({
+      method: "GET",
+      url: "/roles",
+      auth: {
+        strategy: "entra",
+        credentials: {
+          user: User.createMock(),
+        },
+      },
+    });
+
+    expect(statusCode).toEqual(403);
+  });
+
+  it("returns all roles when user is admin", async () => {
     const roles = [Role.createMock(), Role.createMock()];
+    const admin = User.createMock({
+      idpRoles: ["FCP.Casework.Admin"],
+    });
 
     findRolesUseCase.mockResolvedValue(roles);
 
     const { statusCode, result } = await server.inject({
       method: "GET",
       url: "/roles",
+      auth: {
+        strategy: "entra",
+        credentials: {
+          user: admin,
+        },
+      },
     });
 
     expect(statusCode).toEqual(200);
     expect(result).toEqual(roles);
-
-    expect(findRolesUseCase).toHaveBeenCalled();
+    expect(findRolesUseCase).toHaveBeenCalledWith({
+      user: admin,
+    });
   });
 
   it("validates response using findRolesResponseSchema", () => {
