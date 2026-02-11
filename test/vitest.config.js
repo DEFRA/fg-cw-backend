f191fe96e2b1ddb0447acf482629357f576b2317
julian <juliankimmings@gmail.com>
2026-02-11 15:20:35 +0000

    update queue names

--- test/vitest.config.js
+++ test/vitest.config.js
@@ -28,8 +28,8 @@ export default defineConfig({
       AWS_ACCESS_KEY_ID: "test",
       AWS_SECRET_ACCESS_KEY: "test",
       CW__SQS__CREATE_NEW_CASE_URL: `${SQS_URL}/cw__sqs__create_new_case_fifo.fifo`,
-      CW__SQS__UPDATE_STATUS_URL: `${SQS_URL}/cw__sqs__update_case_status_fifo.fifo`,
-      GAS__SQS__UPDATE_STATUS: `${SQS_URL}/gas__sqs__update_status_fifo.fifo`,
+      CW__SQS__UPDATE_STATUS_URL: `${SQS_URL}/cw__sqs__update_status_fifo.fifo`,
+      GAS__SQS__UPDATE_STATUS: `${SQS_URL}/gas__sqs__update_status_fifo.fifo`, // required for varification purposes only
       OIDC_JWKS_URI: `http://localhost:${ENTRA_PORT}/jwks`,
       OIDC_VERIFY_ISS: `http://localhost:3010`, // Match the actual token issuer from Entra stub
       OIDC_VERIFY_AUD: "api://client1",
