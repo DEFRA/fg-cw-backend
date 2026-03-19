10186e202302a4f70be7eb0f99d4475682619aa6
julian <juliankimmings@gmail.com>
2026-03-19 17:02:33 +0000

    case and workflow closes migration

---
+++ migrations/20260319142144-add-workflow-closed-flag.js
@@ -0,0 +1,42 @@
+import { withTransaction } from "../src/common/with-transaction.js";
+
+export const up = async (db) => {
+  await withTransaction(async (session) => {
+    await db.collection("cases").updateMany(
+      {},
+      [
+        {
+          $set: {
+            closed: {
+              $ifNull: ["$closed", false],
+            },
+            closedAt: null,
+          },
+        },
+      ],
+      { session },
+    );
+
+    const workflows = await db.collection("workflows").find({}).toArray();
+    for (const workflow of workflows) {
+      for (const phase of workflow.phases) {
+        for (const stage of phase.stages) {
+          stage.statuses = stage.statuses.map((status) => {
+            return {
+              ...status,
+              closes: false,
+            };
+          });
+        }
+      }
+
+      await db
+        .collection("workflows")
+        .updateOne(
+          { _id: workflow._id },
+          { $set: { phases: workflow.phases } },
+          { session },
+        );
+    }
+  });
+};
