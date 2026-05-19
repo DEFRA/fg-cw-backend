1dbdb5bb4fdfe989ae77ca14937d3b965eef6bed
julian <juliankimmings@gmail.com>
2026-05-19 13:37:54 +0100

    pop the heading from the content list

--- migrations/20260519095606-update-termination-label.js
+++ migrations/20260519095606-update-termination-label.js
@@ -2,19 +2,35 @@ export const up = async (db) => {
   const terminationComment = {
     label: {
       text: "Reason for termination",
-      classes: "",
+      classes: "govuk-label--s",
     },
     helpText: "You must include an explanation for auditing purposes.",
     mandatory: true,
   };
 
-  await db.collection("workflows").updateOne(
-    { code: "frps-private-beta" },
-    {
-      $set: {
-        "phases.1.stages.0.statuses.0.transitions.0.action.comment":
-          terminationComment,
-      },
+  const workflow = await db
+    .collection("workflows")
+    .findOne({ code: "frps-private-beta" });
+
+  if (!workflow) {
+    return;
+  }
+
+  const content =
+    workflow.phases?.[1]?.stages?.[0]?.beforeContent?.[0]?.content;
+
+  const update = {
+    $set: {
+      "phases.1.stages.0.statuses.0.transitions.0.action.comment":
+        terminationComment,
     },
-  );
+  };
+
+  if (content?.length === 6) {
+    update.$pop = { "phases.1.stages.0.beforeContent.0.content": 1 };
+  }
+
+  await db
+    .collection("workflows")
+    .updateOne({ code: "frps-private-beta" }, update);
 };
