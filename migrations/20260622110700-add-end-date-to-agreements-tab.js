83066132ea15c1056f900cddd0ac01b27b20210a
asifshaik04 <45974468+asifshaik04@users.noreply.github.com>
2026-06-22 11:55:18 +0100

    FGP-1129: Displaying end date for WMP workflow
    FGP-1129: Displaying end date for WMP workflow

---
+++ migrations/20260622110700-add-end-date-to-agreements-tab.js
@@ -0,0 +1,31 @@
+export const up = async (db) => {
+  await db.collection("workflows").updateOne(
+    { code: "woodland" },
+    {
+      $push: {
+        "pages.cases.details.tabs.agreements.content.1.rows": {
+          $each: [
+            {
+              label: "End date",
+              text: "jsonata:$exists($max($.supplementaryData.agreements[agreementStatus='ACCEPTED'].$toMillis(endDate))) ? $fromMillis($max($.supplementaryData.agreements[agreementStatus='ACCEPTED'].$toMillis(endDate)), '[D] [MNn,*-3] [Y]') : 'Not started'",
+            },
+          ],
+          $position: 5,
+        },
+      },
+    },
+  );
+};
+
+export const down = async (db) => {
+  await db.collection("workflows").updateOne(
+    { code: "woodland" },
+    {
+      $pull: {
+        "pages.cases.details.tabs.agreements.content.1.rows": {
+          label: "End date",
+        },
+      },
+    },
+  );
+};
