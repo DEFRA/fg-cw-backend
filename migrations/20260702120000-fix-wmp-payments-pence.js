b9ba8468e836763b6c14a70bf75f284d5e1fd14c
Justin Esynergy <justin.ramel@esynergy.co.uk>
2026-07-02 11:31:20 +0100

    Use regex for event type matching in WMP payments migration

--- migrations/20260702120000-fix-wmp-payments-pence.js
+++ migrations/20260702120000-fix-wmp-payments-pence.js
@@ -1,6 +1,8 @@
 const CASE_REF = "wmp-2ax-726";
 const WORKFLOW_CODE = "woodland";
 const BAD_PENCE = { $gt: 238769, $lt: 238770 };
+const CASE_CREATE_EVENT_TYPE =
+  /^cloud\.defra\.[^.]+\.fg-gas-backend\.case\.create$/;
 const FIXED_PENCE = 238770;
 
 export const up = async (db) => {
@@ -21,7 +23,7 @@ export const up = async (db) => {
   await db.collection("inbox").updateOne(
     {
       source: "GAS",
-      "event.type": "cloud.defra.dev.fg-gas-backend.case.create",
+      "event.type": CASE_CREATE_EVENT_TYPE,
       "event.data.caseRef": CASE_REF,
       "event.data.workflowCode": WORKFLOW_CODE,
       "event.data.payload.answers.totalAgreementPaymentPence": BAD_PENCE,
