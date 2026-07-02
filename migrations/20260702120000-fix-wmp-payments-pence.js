9ccda0018394faa0997906f28b17c8ae88f52c70
Justin Esynergy <justin.ramel@esynergy.co.uk>
2026-07-02 11:25:21 +0100

    Fix incorrect total agreement payment value in WMP cases and inbox events

---
+++ migrations/20260702120000-fix-wmp-payments-pence.js
@@ -0,0 +1,37 @@
+const CASE_REF = "wmp-2ax-726";
+const WORKFLOW_CODE = "woodland";
+const BAD_PENCE = { $gt: 238769, $lt: 238770 };
+const FIXED_PENCE = 238770;
+
+export const up = async (db) => {
+  await db.collection("cases").updateOne(
+    {
+      caseRef: CASE_REF,
+      workflowCode: WORKFLOW_CODE,
+      "payload.answers.totalAgreementPaymentPence": BAD_PENCE,
+    },
+    {
+      $set: {
+        "payload.answers.totalAgreementPaymentPence": FIXED_PENCE,
+        "payload.answers.payments.agreement.0.agreementTotalPence": FIXED_PENCE,
+      },
+    },
+  );
+
+  await db.collection("inbox").updateOne(
+    {
+      source: "GAS",
+      "event.type": "cloud.defra.dev.fg-gas-backend.case.create",
+      "event.data.caseRef": CASE_REF,
+      "event.data.workflowCode": WORKFLOW_CODE,
+      "event.data.payload.answers.totalAgreementPaymentPence": BAD_PENCE,
+    },
+    {
+      $set: {
+        "event.data.payload.answers.totalAgreementPaymentPence": FIXED_PENCE,
+        "event.data.payload.answers.payments.agreement.0.agreementTotalPence":
+          FIXED_PENCE,
+      },
+    },
+  );
+};
