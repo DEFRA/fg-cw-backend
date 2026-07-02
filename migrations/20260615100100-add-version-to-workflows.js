04b549b7224e1bf420454c62a81f52e565f136bc
Aditya Kolnati <adityakolnati@Adityas-Mac-Pro-M2-Max-Chip.local>
2026-07-02 14:36:41 +0100

    consolidate migration scripts

--- migrations/20260615100100-add-version-to-workflows.js
+++
@@ -1,5 +0,0 @@
-export const up = async (db) => {
-  const workflows = db.collection("workflows");
-  await workflows.createIndex({ code: 1, version: 1 }, { unique: true });
-  await workflows.dropIndex("code_1");
-};
