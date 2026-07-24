c24aabe638be2c007e4e1ffe6a8622f33f6e937f
julian <juliankimmings@gmail.com>
2026-07-24 11:21:21 +0100

    merge migrations to single file

--- migrations/20260722134509-rename-status-to-value.js
+++ migrations/20260722134509-rename-status-to-value.js
@@ -31,5 +31,35 @@ export const up = async (db) => {
           );
       }
     }
+
+    const workflows = await db.collection("workflows").find({}).toArray();
+
+    for (const workflow of workflows) {
+      let updated = false;
+
+      for (const phase of workflow.phases || []) {
+        for (const stage of phase.stages || []) {
+          for (const taskGroup of stage.taskGroups || []) {
+            for (const task of taskGroup.tasks || []) {
+              if (task.statusOptions !== undefined) {
+                task.valueOptions = task.statusOptions;
+                delete task.statusOptions;
+                updated = true;
+              }
+            }
+          }
+        }
+      }
+
+      if (updated) {
+        await db
+          .collection("workflows")
+          .updateOne(
+            { _id: workflow._id },
+            { $set: { phases: workflow.phases } },
+            { session },
+          );
+      }
+    }
   });
 };
